808a1aa10a3bc39d6b62a09ef1662cc5136cd7d5
malak <malek.bendhia@fsb.ucar.tn>
2024-09-16 00:18:29 +0100

    number section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       },
       backgroundImage: {
         'custom-gradient': 'linear-gradient(90deg, #07233B 1.67%, #041D34 39.95%, #04192D 58.24%, #040E18 80.28%, #050C16 101.48%)',
+        'number-custom-gradient': 'linear-gradient(90deg, rgb(7, 35, 70) 2%, #072D4E 39.95%, #072D4E 58.24%, #051A2F 80.28%, #051A2F 101.48%)',
         'number-section-background':"url('/assets/images/background image.jpg')"
       }
     },
